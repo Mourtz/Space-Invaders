bd0688b1038aaa8870d235054603d8b8e3620832
Alex Mourtziapis <alex.mourtziapis@gmail.com>
2015-07-04 22:51:24 +0300

    Added ignorant enemies.

--- main.c
+++ main.c
@@ -22,6 +22,9 @@
 #include <unistd.h>
 #include <pthread.h>  // POSIX theading
 
+// https://en.wikipedia.org/wiki/Braille_Patterns#Chart
+const char* charcode[5] = {" ", "\u2801", "\u28FF" , "\u2847", "\u2809"};
+
 WINDOW *canvas;
 
 int **pixel_matrix;
@@ -34,22 +37,7 @@ void draw(){
 
   for(x=0;x<max_x;x++){
     for(y=0;y<max_y;y++){
-      switch (pixel_matrix[x][y]) {
-        case 0:
-          mvwprintw(canvas, y, x, " ");
-          // mvprintw(y, x, " ");
-          break;
-        case 1:
-          mvwprintw(canvas, y, x, "\u2801");
-          // mvprintw(y, x, MIDDOT);
-          break;
-        case 2:
-          mvwprintw(canvas, y, x, "\u28FF");
-          // mvprintw(y, x, MIDDOT);
-          break;
-        default:
-          break;
-      }
+      mvwprintw(canvas, y, x, charcode[pixel_matrix[x][y]]);
       // usleep(300);
     }
   }
@@ -128,10 +116,149 @@ void draw_rect(int _x0, int _y0, int _width, int _height, int value){
 
 void draw_border(){
   // border
-  draw_line(-1,0,max_x-1,0, 1);
-  draw_line(-1,max_y-1,max_x-1,max_y-1, 1);
-  draw_line(0,0,0,max_y-1, 1);
-  draw_line(max_x-1,0,max_x-1,max_y-1, 1);
+  draw_line(-1,0,max_x-1,0, 4);
+  draw_line(-1,max_y-1,max_x-1,max_y-1, 4);
+  draw_line(0,-1,0,max_y-2, 3);
+  draw_line(max_x-1,-1,max_x-1,max_y-2, 3);
+}
+
+//////////////////////////////////////////////////////////////////////////
+// ENEMY
+//////////////////////////////////////////////////////////////////////////
+
+struct _enemy{
+  unsigned int posX, posY;    // enemy's postion
+  unsigned int width, height; // the actual scale of enemy
+  unsigned int health;        // enemy's health (0-100)
+  char type;                  // enemy type
+  char moveDir;
+};
+//    A type Enemies
+struct _enemy enemy1 = {0, 0, 0, 0, 100, 'a', 'r'};struct _enemy enemy2 = {0, 0, 0, 0, 100, 'a', 'r'};
+struct _enemy enemy3 = {0, 0, 0, 0, 100, 'a', 'r'};struct _enemy enemy4 = {0, 0, 0, 0, 100, 'a', 'r'};
+struct _enemy enemy5 = {0, 0, 0, 0, 100, 'a', 'r'};struct _enemy enemy6 = {0, 0, 0, 0, 100, 'a', 'r'};
+struct _enemy *enemA[] = {&enemy1, &enemy2, &enemy3, &enemy4, &enemy5, &enemy6};
+//    B type enemies
+struct _enemy enemy7 = {0, 0, 0, 0, 100, 'b', 'l'};struct _enemy enemy8 = {0, 0, 0, 0, 100, 'b', 'l'};
+struct _enemy enemy9 = {0, 0, 0, 0, 100, 'b', 'l'};struct _enemy enemy10 = {0, 0, 0, 0, 100, 'b', 'l'};
+struct _enemy enemy11 = {0, 0, 0, 0, 100, 'b', 'l'};struct _enemy enemy12 = {0, 0, 0, 0, 100, 'b', 'l'};
+struct _enemy *enemB[] = {&enemy7, &enemy8, &enemy9, &enemy10, &enemy11, &enemy12};
+//    C type enemies
+struct _enemy enemy13 = {0, 0, 0, 0, 100, 'c', 'r'};struct _enemy enemy14 = {0, 0, 0, 0, 100, 'c', 'r'};
+struct _enemy enemy15 = {0, 0, 0, 0, 100, 'c', 'r'};struct _enemy enemy16 = {0, 0, 0, 0, 100, 'c', 'r'};
+struct _enemy enemy17 = {0, 0, 0, 0, 100, 'c', 'r'};struct _enemy enemy18 = {0, 0, 0, 0, 100, 'c', 'r'};
+struct _enemy *enemC[] = {&enemy13, &enemy14, &enemy15, &enemy16, &enemy17, &enemy18};
+void enemy_draw(){
+  for(int i=0,z=0; i<18; i++){
+    if(i<6){
+      enemA[i]->width = abs(max_x/18);
+      enemA[i]->height = abs(enemA[i]->width/2);
+      enemA[i]->posX = abs(enemA[i]->width/2)+(i*abs(enemA[i]->width*2.0));
+      enemA[i]->posY = abs(enemA[i]->height/2);
+      draw_rect(enemA[i]->posX ,enemA[i]->posY, enemA[i]->width, enemA[i]->height, 2);
+    }
+    else if(i<12){
+      z=i;
+      i-=6;
+
+      enemB[i]->width = abs(max_x/18);
+      enemB[i]->height = abs(enemB[i]->width/2);
+      enemB[i]->posX = abs(enemB[i]->width/2)+(i*abs(enemB[i]->width*2.0));
+      enemB[i]->posY = abs(enemB[i]->height/2)+abs(enemB[i]->height*1.5);;
+      draw_rect(enemB[i]->posX ,enemB[i]->posY, enemB[i]->width, enemB[i]->height, 2);
+
+      i=z;
+    }
+    else{
+      z=i;
+      i-=12;
+
+      enemC[i]->width = abs(max_x/18);
+      enemC[i]->height = abs(enemC[i]->width/2);
+      enemC[i]->posX = abs(enemC[i]->width/2)+(i*abs(enemC[i]->width*2.0));
+      enemC[i]->posY = abs(enemC[i]->height/2)+abs(enemC[i]->height*3.0);;
+      draw_rect(enemC[i]->posX ,enemC[i]->posY, enemC[i]->width, enemC[i]->height, 2);
+
+      i=z;
+    }
+  }
+}
+void enemy_clear(){
+  for(int i=0,z=0; i<18; i++){
+    if(i<6){
+      draw_rect(enemA[i]->posX ,enemA[i]->posY, enemA[i]->width, enemA[i]->height, 0);
+    }
+    else if(i<12){
+      z=i;i-=6;
+      draw_rect(enemB[i]->posX ,enemB[i]->posY, enemB[i]->width, enemB[i]->height, 0);
+      i=z;
+    }
+    else{
+      z=i;i-=12;
+      draw_rect(enemC[i]->posX ,enemC[i]->posY, enemC[i]->width, enemC[i]->height, 0);
+      i=z;
+    }
+  }
+}
+void enemy_update() {
+  enemy_clear();
+  for(int i=0,z=0; i<18; i++){
+    if(i<6){
+      if(enemA[i]->moveDir == 'r'){
+        if((enemA[5]->posX + enemA[5]->width) == (max_x-2)){
+          enemA[i]->moveDir = 'l';
+        }else{
+          enemA[i]->posX ++;
+        }
+      }
+      else{
+        if(enemA[0]->posX == 0){
+          enemA[i]->moveDir = 'r';
+        }else{
+          enemA[i]->posX --;
+        }
+      }
+      draw_rect(enemA[i]->posX ,enemA[i]->posY, enemA[i]->width, enemA[i]->height, 2);
+    }
+    else if(i<12){
+      z=i;i-=6;
+      if(enemB[i]->moveDir == 'r'){
+        if((enemB[5]->posX + enemB[5]->width) == (max_x-2)){
+          enemB[i]->moveDir = 'l';
+        }else{
+          enemB[i]->posX ++;
+        }
+      }
+      else{
+        if(enemB[0]->posX == 0){
+          enemB[i]->moveDir = 'r';
+        }else{
+          enemB[i]->posX --;
+        }
+      }
+      draw_rect(enemB[i]->posX ,enemB[i]->posY, enemB[i]->width, enemB[i]->height, 2);
+      i=z;
+    }
+    else{
+      z=i;i-=12;
+      if(enemC[i]->moveDir == 'r'){
+        if((enemC[5]->posX + enemC[5]->width) == (max_x-2)){
+          enemC[i]->moveDir = 'l';
+        }else{
+          enemC[i]->posX ++;
+        }
+      }
+      else{
+        if(enemC[0]->posX == 0){
+          enemC[i]->moveDir = 'r';
+        }else{
+          enemC[i]->posX --;
+        }
+      }
+      draw_rect(enemC[i]->posX ,enemC[i]->posY, enemC[i]->width, enemC[i]->height, 2);
+      i=z;
+    }
+  }
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -139,14 +266,15 @@ void draw_border(){
 //////////////////////////////////////////////////////////////////////////
 
 struct _obstacle{
-  unsigned int posX, posY;    // the position of the obstacle
-  unsigned int width, height; // the actual scale of the obsacle
-  unsigned int *health;       // health of each "pixel" consisting the obstacle
+  unsigned int posX, posY;    // obstacles position
+  unsigned int width, height; // the actual scale of obsacle
+  int *health;       // health of each "pixel" consisting the obstacle
 };
-struct _obstacle obstacle1 = {0, 0, 20, 6};
-struct _obstacle obstacle2 = {0, 0, 20, 6};
-struct _obstacle obstacle3 = {0, 0, 20, 6};
-struct _obstacle obstacle4 = {0, 0, 20, 6};
+
+struct _obstacle obstacle1 = {0, 0, 0, 0};
+struct _obstacle obstacle2 = {0, 0, 0, 0};
+struct _obstacle obstacle3 = {0, 0, 0, 0};
+struct _obstacle obstacle4 = {0, 0, 0, 0};
 struct _obstacle *obst[] = {&obstacle1, &obstacle2, &obstacle3, &obstacle4};
 void obstacle_draw(){
   for(int i=0; i<4; i++){
@@ -165,7 +293,6 @@ void obstacle_draw(){
     draw_rect(obst[i]->posX ,obst[i]->posY, obst[i]->width, obst[i]->height, 2);
   }
 }
-
 void obstacle_damage(int x, int y, int n){
   for(int i=obst[n]->posY; i<=obst[n]->posY+obst[n]->height; i++){
     for(int l=obst[n]->posX; l<=obst[n]->posX+obst[n]->width; l++){
@@ -173,7 +300,7 @@ void obstacle_damage(int x, int y, int n){
         int ii = i - obst[n]->posY;
         int ll = l - obst[n]->posX;
 
-        obst[n]->health[obst[n]->width*ii + ll] -= 50;
+        obst[n]->health[obst[n]->width*ii + ll] -= 10;
         if(obst[n]->health[obst[n]->width*ii + ll] <= 0){
           pixel_matrix[x][y]=0;
         }
@@ -182,7 +309,6 @@ void obstacle_damage(int x, int y, int n){
     }
   }
 }
-
 int obstacle_inBounds(int x){
   for(int i=0; i<4; i++){
     if(x<=obst[i]->posX+obst[i]->width && x>obst[i]->posX )
@@ -196,20 +322,22 @@ int obstacle_inBounds(int x){
 //////////////////////////////////////////////////////////////////////////
 
 struct _spacecraft{
-  unsigned int posX, posY;
-  unsigned int width, height;
-  unsigned int health;  // [0,100]
+  unsigned int posX, posY;    // spacecrafts position
+  unsigned int width, height; //  the actual scale of spacecraft
+  unsigned int health;        // spacecrafts health (0-100)
   bool hasfired;
 };
+
 struct _spacecraft spacecraft = {0, 0, 7, 2, 100, false};
 struct _spacecraft *spcr = &spacecraft;
+
 // draw spacecraft on screen
 void draw_spacecraft(){
   spcr->posX = abs(max_x/2)-abs(spcr->width/2);
   spcr->posY = abs(max_y)-2*spcr->height;
 
-  draw_rect(spcr->posX ,spcr->posY, spcr->width, spcr->height, 1);
-  draw_line(spcr->posX + spcr->width/3, spcr->posY-1, spcr->posX + 2*spcr->width/3+1, spcr->posY-1, 1);
+  draw_rect(spcr->posX ,spcr->posY, spcr->width, spcr->height, 2);
+  draw_line(spcr->posX + spcr->width/3, spcr->posY-1, spcr->posX + 2*spcr->width/3+1, spcr->posY-1, 2);
   return;
 }
 // erase spacecraft from canvas
@@ -223,8 +351,8 @@ void *spacecraft_moveR(){
   spacecraft_clear();
   spcr->posX ++;
 
-  draw_rect(spcr->posX ,spcr->posY, spcr->width, spcr->height, 1);
-  draw_line(spcr->posX + spcr->width/3, spcr->posY-1, spcr->posX + 2*spcr->width/3+1, spcr->posY-1, 1);
+  draw_rect(spcr->posX ,spcr->posY, spcr->width, spcr->height, 2);
+  draw_line(spcr->posX + spcr->width/3, spcr->posY-1, spcr->posX + 2*spcr->width/3+1, spcr->posY-1, 2);
   return;
 }
 // move the spacecraft to the left
@@ -232,8 +360,8 @@ void *spacecraft_moveL(){
   spacecraft_clear();
   spcr->posX --;
 
-  draw_rect(spcr->posX ,spcr->posY, spcr->width, spcr->height, 1);
-  draw_line(spcr->posX + spcr->width/3, spcr->posY-1, spcr->posX + 2*spcr->width/3+1, spcr->posY-1, 1);
+  draw_rect(spcr->posX ,spcr->posY, spcr->width, spcr->height, 2);
+  draw_line(spcr->posX + spcr->width/3, spcr->posY-1, spcr->posX + 2*spcr->width/3+1, spcr->posY-1, 2);
   return;
 }
 void *spacecraft_fire(int value){
@@ -310,6 +438,7 @@ int main(int argc, char *argv[])
   draw_border();
   draw_spacecraft();
   obstacle_draw();
+  enemy_draw();
 
   while(true){
     draw();
@@ -320,28 +449,36 @@ int main(int argc, char *argv[])
       if(ch == '#'){
         break;
       }
+      // UP ARROW
       else if(ch == KEY_UP){
         if(spcr->hasfired == false){
-          spacecraft_fire (1);
+          spacecraft_fire (4);
           spcr->hasfired = true;
         }
       }
+      // DOWN ARROW
       else if(ch == KEY_DOWN){
 
       }
+      // LEFT ARROW
       else if(ch == KEY_LEFT){
-        pthread_create(&tid, NULL, spacecraft_moveL, NULL);
+        spacecraft_moveL();
         if(spcr->hasfired == true){
           spacecraft_fire (0);
           spcr->hasfired = false;
         }
       }
+      // RIGHT ARROW
       else if(ch == KEY_RIGHT){
-        pthread_create(&tid, NULL, spacecraft_moveR, NULL);
+        spacecraft_moveR();
         if(spcr->hasfired == true){
           spacecraft_fire (0);
           spcr->hasfired = false;
         }
+      }
+      // SPACEBAR
+      else if(ch == ' '){
+        //pthread_create(&tid, NULL, enemy_update, NULL);
       }
       else
       {
@@ -354,17 +491,18 @@ int main(int argc, char *argv[])
         spcr->hasfired = false;
       }
     }
+    pthread_create(&tid, NULL, enemy_update, NULL);
     usleep(20000);
   }
 
 
-printw("\n\nFarewell!\n");
+  printw("\n\nFarewell!\n");
 
-refresh();
-getch();
-endwin();
+  refresh();
+  getch();
+  endwin();
 
-pthread_exit(NULL);
+  pthread_exit(NULL);
 
-return 0;
+  return 0;
 }
